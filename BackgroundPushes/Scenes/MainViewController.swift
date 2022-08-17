5d075c003ab855bafbbf46bc2ba23a19c3a77000
Jaewon <woin2ee@gmail.com>
2022-08-17 18:43:33 +0900

    Notification 등록 버튼 구현

--- BackgroundPushes/Scenes/MainViewController.swift
+++ BackgroundPushes/Scenes/MainViewController.swift
@@ -6,10 +6,32 @@
 //
 
 import UIKit
+import OSLog
 
 final class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func didTapRegisterNotificationButton(_ sender: UIButton) {
+        let notification = UNMutableNotificationContent.init()
+        notification.title = "Title"
+        notification.subtitle = "subtitle"
+        notification.body = "body"
+        notification.sound = .default
+        
+        let timeInterval: TimeInterval = 5.0
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: timeInterval, repeats: false)
+        
+        let identifier = "LocalNotification"
+        let request = UNNotificationRequest.init(
+            identifier: identifier,
+            content: notification,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request)
+        Logger.test.notice("알림 등록 완료(\(timeInterval, format: .hybrid)초 후 알림, Identifier: \(identifier))")
     }
 }
