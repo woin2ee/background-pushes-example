8964469435ddf118564b581ac76bd35f65396dd8
Jaewon <woin2ee@gmail.com>
2022-08-17 18:37:47 +0900

    권한 요청 구현

--- BackgroundPushes/Application/AppDelegate.swift
+++ BackgroundPushes/Application/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import OSLog
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Logger.test.notice("didFinishLaunching")
+        self.requestNotificationPermission()
         return true
     }
     
@@ -30,7 +30,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    
 }
 
+private extension AppDelegate {
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: [.alert, .sound, .badge],
+            completionHandler: { didAllow, error in
+                if didAllow {
+                    Logger.test.notice("알림 허용 상태")
+                } else {
+                    Logger.test.notice("알림 거부 상태")
+                }
+            }
+        )
+    }
+}
